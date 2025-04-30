29a93fb46dac9a7806d8537023ecdd41ee638daf
Deoshabh <gagan2111015@akgec.ac.in>
2025-04-30 15:44:00 +0530

    fix

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "http://so88s4g4o8cgwscsosk448kw.147.79.66.75.sslip.io/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
