8f68d227df65feb87e3aa29b24caf08fabd16e94
Deoshabh <gagan2111015@akgec.ac.in>
2025-05-12 13:18:13 +0530

    config

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
+import webpack from 'webpack';
 
-module.exports = function override(config, env) {
+export default function override(config) {
+  // Remove ModuleScopePlugin to allow imports outside of src/
+  config.resolve.plugins = config.resolve.plugins.filter(plugin => 
+    plugin.constructor.name !== 'ModuleScopePlugin'
+  );
+
   // Configure resolve for Node.js polyfills and module resolution
   config.resolve = {
     ...config.resolve,
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src')
-    ],
     fallback: {
       "https": require.resolve('https-browserify'),
-      "http": require.resolve('stream-http'),
+      "http": require.resolve('stream-http'), 
       "stream": require.resolve('stream-browserify'),
       "util": require.resolve('util/'),
       "zlib": require.resolve('browserify-zlib'),
@@ -22,6 +22,15 @@ module.exports = function override(config, env) {
       "path": false,
       "fs": false,
       "os": false
+    },
+    alias: {
+      'https-browserify': require.resolve('https-browserify'),
+      'stream-http': require.resolve('stream-http'),  
+      'stream-browserify': require.resolve('stream-browserify'),
+      'zlib': require.resolve('browserify-zlib'),
+      'url': require.resolve('url'),
+      'assert': require.resolve('assert'),
+      'buffer': require.resolve('buffer')
     }
   };
 
