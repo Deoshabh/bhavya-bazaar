3f8b0d8104009b1ec67900676c437bd224800e90
Deoshabh <gagan2111015@akgec.ac.in>
2025-06-10 00:45:27 +0530

    fix

--- frontend/src/pages/ShopCreate.jsx
+++ frontend/src/pages/ShopCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import ShopCreate from "../components/Shop/ShopCreate";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -10,7 +10,7 @@ const ShopCreatePage = () => {
     // if seller is already logged in, redirect to shop dashboard
     useEffect(() => {
         if (isSeller === true && seller?._id) {
-            navigate(`/shop/${seller._id}`);
+            navigate("/dashboard");
         }
     }, [isSeller, seller, navigate]);
     return (
