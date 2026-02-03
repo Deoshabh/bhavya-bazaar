777e26a106902cd88af29a4df5097ab642532edb
GAGAN DEV SINGH <gagan2111015@akgec.ac.in>
2026-02-03 18:45:45 +0530

    fix

--- frontend/src/components/Admin/AdminManagement.jsx
+++ frontend/src/components/Admin/AdminManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
+import { server } from "../../server";
 import { 
   FiEdit3, 
   FiTrash2, 
